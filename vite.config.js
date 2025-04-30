690139dd1454b761ab238e3ccebf7a5335f8b897
Egorek231 <gstepanskij@gmail.com>
2025-04-30 12:02:01 +0300

    йцу

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: process.env.PORT || 5173,
-    allowedHosts: ["project-2jmx.onrender.com"||"all"]
+    allowedHosts: ["project-p609.onrender.com"||"*"]
   }
 })
